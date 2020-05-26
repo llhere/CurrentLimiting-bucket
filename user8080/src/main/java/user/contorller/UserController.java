d07db41d7cacbbe577d05ee3e4a9388165be43ed
chen <1259606144@qq.com>
2020-05-26 22:58:30 +0800

    1

--- user8080/src/main/java/user/contorller/UserController.java
+++ user8080/src/main/java/user/contorller/UserController.java
@@ -28,19 +28,13 @@ public class UserController {
     public String m1(@PathVariable Long id) {
         //渠道100、机构100、服务a  key=100100a ，100，10，100
         //渠道101、机构101、服务a  key =101101a，10，10，100
-//        string  ABC =geteRatelimite(渠道、机构、服务);
-//        if(ABC){
-//            http://www.baiduw.com
-//        }else{
-//            http://erroe.html
-//        }
 
         //调用的服务
-        String a = (1 == id) ? "111222333" : "222333444";
+        String client = (1 == id) ? "111222333" : "222333444";
 
 
         //服务A配置
-        Map bucketConfigMap = redisTemplate.opsForHash().entries("rateLimter:" + a);
+        Map bucketConfigMap = redisTemplate.opsForHash().entries("rateLimter:" + client);
 
         //配置不为空
         if (bucketConfigMap.size() > 0) {
@@ -49,7 +43,7 @@ public class UserController {
             String msg = "当前程序：" + bucketConfigMap.get("app") + ",消费后令牌:" + bucketConfigMap.get("stored_permits") + ",最多令牌：" + bucketConfigMap.get("max_permits") + ",放入一个令牌时间间隔：" + bucketConfigMap.get("interval");
 
             //调用脚本并执行 执行结果为false则没有获取到令牌
-            if (!execute(a, msg)){
+            if (!execute(client, msg)){
                 return "null";
             }
         }
@@ -65,10 +59,10 @@ public class UserController {
      * @author chenpengwei
      * @date 2020/5/26 下午 8:12
      */
-    private boolean execute(String a, String msg) {
+    private boolean execute(String client, String msg) {
 
         //执行获取令牌
-        RateLimitResult result = rateLimitClient.acquire(a);
+        RateLimitResult result = rateLimitClient.acquire(client);
 
         //获取结果
         if (result == RateLimitResult.ACQUIRE_FAIL) {
